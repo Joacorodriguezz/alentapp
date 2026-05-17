9e74b77f8b60260663618f6c0670161b77f955d3
lucaslegor <legorlucas81@gmail.com>
2026-05-17 19:47:56 -0300

    hotfix(layout): corrige problemas con el layout, cierre de etiquetas

--- packages/web/src/routes.ts
+++ packages/web/src/routes.ts
@@ -1,46 +1,21 @@
 import { createBrowserRouter } from "react-router";
 
-import { MembersView } from "./views/Members";
-import { LockersView } from "./views/Lockers";
-import { HomeView } from "./views/Home";
 import Layout from "./Layout";
-import { MembersView } from "./views/Members";
 import { DisciplinesView } from "./views/Disciplines";
-import { PaymentsView } from "./views/Payments";
 import { HomeView } from "./views/Home";
-import Layout from "./Layout";
+import { LockersView } from "./views/Lockers";
+import { MembersView } from "./views/Members";
+import { PaymentsView } from "./views/Payments";
 
-export let router = createBrowserRouter([
+export const router = createBrowserRouter([
   {
     Component: Layout,
     children: [
-      {
-        path: "/",
-        Component: HomeView,
-      },
-      {
-        path: "/members",
-        Component: MembersView,
-      },
-      {
-        path: "/lockers",
-        Component: LockersView,
-      },
-    ],
-  },
-]);
-      {
-        path: "/members",
-        Component: MembersView,
-      },
-      {
-        path: "/disciplines",
-        Component: DisciplinesView,
-      },
-      {
-        path: "/payments",
-        Component: PaymentsView,
-      },
+      { path: "/", Component: HomeView },
+      { path: "/members", Component: MembersView },
+      { path: "/lockers", Component: LockersView },
+      { path: "/disciplines", Component: DisciplinesView },
+      { path: "/payments", Component: PaymentsView },
     ],
   },
 ]);
